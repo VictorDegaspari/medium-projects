e8dbfa47650f26f7f58a4bfbf6365defcde94643
VictorDegaspari <vdegaspari.vm@gmail.com>
2021-01-06 16:31:19 -0300

    Add: pessoas resp pela pub, alteracoes no admin

--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'pessoas'
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'project/static')
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
