9c25fe40baab4d5743a30960d31e248ce53d58e3
Victor Degaspari <victor.degaspari@digitalsys.com.br>
2021-01-22 18:59:14 -0300

    login

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,4 @@
+from django.contrib import auth
 from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 
@@ -5,7 +6,7 @@ from django.shortcuts import redirect, render
 def cadastro(request):
     if request.method == 'POST':
         nome = request.POST['nome']
-        email = request.POST['emails']
+        email = request.POST['email']
         senha = request.POST['password']
         senha2 = request.POST['password2']
         if not nome.strip():
@@ -30,14 +31,27 @@ def cadastro(request):
 
 def login(request):
     if request.method == 'POST':
-        print('Usuário criado com sucesso')
-        return redirect('login')
+        email = request.POST['email']
+        senha = request.POST['senha']
+        if email == "" or senha == "":
+            return redirect('login')
+        if User.objects.filter(email=email).exists():
+            nome = User.objects.filter(
+                email=email).values_list('username', flat=True).get()
+            user = auth.authenticate(request, username=nome, password=senha)
+            if user is not None:
+                auth.login(request, user)
+        return redirect('dashboard')
     return render(request, 'usuarios/login.html')
 
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return redirect('index')
 
 
 def dashboard(request):
-    pass
+    if request.user.is_authenticated:
+        return render(request, 'usuarios/dashboard.html')
+    else:
+        return redirect('index')
